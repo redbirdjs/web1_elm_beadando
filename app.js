6a3f6fe8b50b4685f2befdc1f3d9c3a7a8ecfef1
keitajs <126980311+keitajs@users.noreply.github.com>
2026-03-23 17:51:49 +0100

    feladat1: főoldal animáció

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ function _scrollTo(selectors) {
   const position = element.getBoundingClientRect();
   window.scrollTo({ top: position.top + window.scrollY, behavior: 'smooth' });
 }
+
+const boxes = document.querySelectorAll(".box");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.animation = " box-animation var(--box-animation-duration) ease";
+      entry.target.style.opacity = 1;
+    }
+  })
+}, {
+  rootMargin: "0px 0px -100px 0px", // 👈 EZ A LÉNYEG
+  threshold: 0
+})
+
+boxes.forEach(box => {
+  box.style.opacity = 0
+  observer.observe(box)
+});
