319dea8dd6c0967d1413dab2c098221c539d9626
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2026-03-23 16:40:02 +0100

    feladat5-6: backend header beállítva, adatbázis importálva, methodok funkciói létrehozva

--- php/backend.php
+++ php/backend.php
@@ -1,1 +1,30 @@
 <?php
+require "db.php";
+
+header('Content-Type: application/json');
+
+$method = $_SERVER["REQUEST_METHOD"];
+$input = json_decode(file_get_contents('php://input'), true);
+
+switch ($method) {
+    case 'GET': getHandler($conn); break;
+    case 'POST': postHandler($conn, $input); break;
+    case 'PUT': putHandler($conn, $input); break;
+    case 'delete': deleteHandler($conn, $input); break;
+}
+
+function getHandler($conn) {
+
+}
+
+function postHandler($conn, $input) {
+
+}
+
+function putHandler($conn, $input) {
+
+}
+
+function deleteHandler($conn, $input) {
+
+}
