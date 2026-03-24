c370a6c1b91ffae6932572e46cbbf26997403d53
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2026-03-24 15:32:52 +0100

    feladat5: funkció a táblázat frissítésére

--- 5_fetchapi_crud/fetchapi.js
+++ 5_fetchapi_crud/fetchapi.js
@@ -1,3 +1,26 @@
 const data = [];
 const tableBody = document.getElementById("data-rows");
 
+function refreshTable(data) {
+    tableBody.innerHTML = "";
+    for (const row of data) {
+        const tr = document.createElement("tr");
+        for (const key in row) {
+            const td = document.createElement("td");
+            td.textContent = row[key];
+            tr.appendChild(td);
+        }
+
+        const buttons = document.createElement("td");
+        const modifyBtn = document.createElement("button");
+        modifyBtn.innerHTML = "Módosítás";
+        buttons.appendChild(modifyBtn);
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = "Törlés";
+        buttons.appendChild(deleteBtn);
+
+        tr.appendChild(buttons);
+        tableBody.appendChild(tr);
+    }
+}
+
