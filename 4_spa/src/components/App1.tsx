b6ef12f1f5d8345d17e52b7ae0eb2dba830e4fa0
keitajs <126980311+keitajs@users.noreply.github.com>
2026-03-30 22:44:23 +0200

    feladat4: Fuel Calculator Inputs/Results rész cserélve a komponensekre; feleslegessé vált részek törölve

--- 4_spa/src/components/App1.tsx
+++ 4_spa/src/components/App1.tsx
@@ -1,5 +1,6 @@
 import { useMemo, useState } from 'react';
-import Input from './App1/Input';
+import Inputs from './App1/Inputs';
+import Results from './App1/Results';
 import './App1.css';
 
 export default function App1() {
@@ -13,30 +14,6 @@ export default function App1() {
   const [maxFuel, setMaxFuel] = useState<string>('');
 
 
-  const setNumberValue = (e: React.ChangeEvent<HTMLInputElement>, set: (value: React.SetStateAction<string>) => void, parse: (str: string) => number) => {
-    let num = parse(e.target.value);
-    if (isNaN(num)) return set('');
-
-    const min = e.target.min ? Number(e.target.min) : NaN;
-    const max = e.target.max ? Number(e.target.max) : NaN;
-
-    if (!isNaN(min) && num < min) num = min;
-    if (!isNaN(max) && num > max) num = max;
-
-    set(String(num));
-  }
-
-  const setIntValue = (e: React.ChangeEvent<HTMLInputElement>, set: (value: React.SetStateAction<string>) => void) => setNumberValue(e, set, parseInt);
-  const setFloatValue = (e: React.ChangeEvent<HTMLInputElement>, set: (value: React.SetStateAction<string>) => void) => setNumberValue(e, set, parseFloat);
-
-
-  const formatTime = (minutes: number) => {
-    const h = Math.floor(minutes / 60);
-    const m = minutes % 60;
-    return h > 0 ? `${h}h ${m}m` : `${m}m`;
-  }
-
-  
   const lapTime = useMemo(() => {
     return (parseInt(min) || 0) + (parseInt(sec) || 0) / 60 + (parseInt(ms) || 0) / 60000 || 1
   }, [min, sec, ms])
@@ -89,111 +66,31 @@ export default function App1() {
   return (
     <div className='fuelcalc'>
 
-      <div className='col'>
-        <div className='row'>
-          <h2>Inputs</h2>
-        </div>
+      <Inputs
+        raceType={raceType}
+        setRaceType={setRaceType}
+        raceLength={raceLength}
+        setRaceLength={setRaceLength}
+        min={min}
+        setMin={setMin}
+        sec={sec}
+        setSec={setSec}
+        ms={ms}
+        setMs={setMs}
+        fuelPerLap={fuelPerLap}
+        setFuelPerLap={setFuelPerLap}
+        maxFuel={maxFuel}
+        setMaxFuel={setMaxFuel}
+      />
 
-        <div className='row'>
-          <h3>Race Setup</h3>
-        </div>
-
-        <div className='row'>
-          <Input
-            label={raceType === 'time' ? 'Race length in minutes' : 'Number of laps'}
-            type='number'
-            name='raceLength'
-            min={0}
-            value={raceLength}
-            onChange={(e) => setIntValue(e, setRaceLength)}
-          />
-
-          <button className={raceType === 'time' ? 'selected' : ''} onClick={() => setRaceType('time')}>Time</button>
-          <button className={raceType === 'laps' ? 'selected' : ''} onClick={() => setRaceType('laps')}>Laps</button>
-        </div>
-
-        {raceType === 'time' &&
-          <div className='row quick-time-select'>
-            {[15, 30, 60, 120, 240, 480, 720, 1440].map(t =>
-              <button className={raceLength === String(t) ? 'selected' : ''} onClick={() => setRaceLength(String(t))}>
-                {t >= 60 ? `${Math.round(t / 60)}h` : `${t}m`}
-              </button>
-            )}
-          </div>
-        }
-
-        <div className='row'>
-          <h3>Lap Time</h3>
-        </div>
-
-        <div className='row'>
-          <Input label='Minutes'     type='number' name='lapTimeM'  min={0} max={9999} value={min} onChange={(e) => setIntValue(e, setMin)} />
-          <Input label='Seconds'     type='number' name='lapTimeS'  min={0} max={59}   value={sec} onChange={(e) => setIntValue(e, setSec)} />
-          <Input label='Miliseconds' type='number' name='lapTimeMs' min={0} max={999}  value={ms}  onChange={(e) => setIntValue(e, setMs)} />
-        </div>
-
-        <div className='row'>
-          <h3>Fuel Consumption</h3>
-        </div>
-
-        <div className='row'>
-          <Input label='Fuel per lap' type='number' name='fuelPerLap' min={0} value={fuelPerLap}  onChange={(e) => setFloatValue(e, setFuelPerLap)} />
-        </div>
-
-        <div className='row'>
-          <Input label='Maximum Fuel (Optional)' type='number' name='maxFuel' min={0} value={maxFuel}  onChange={(e) => setIntValue(e, setMaxFuel)} />
-        </div>
-      </div>
-
-      <div className='col col-w-20'>
-        <div className='row'>
-          <h2>Results</h2>
-        </div>
-
-        <div className='result-row result-main'>
-          <div>Recommended Fuel</div>
-          <div>{recFuel}</div>
-        </div>
-
-        <div className='result-row'>
-          <div>Minimum Fuel</div>
-          <div>{minFuel}</div>
-        </div>
-
-        <div className='result-row'>
-          <div>{raceType === 'time' ? 'Total Laps' : 'Total Time'}</div>
-          <div>{raceType === 'time' ? raceLaps : formatTime(raceTime)}</div>
-        </div>
-
-        {pits.length > 0 && <>
-          <div className='row mt-2'>
-            <h2>Pit Strategy</h2>
-          </div>
-
-          {pits.map((pit, index) => 
-            pit.lap === 0 ?
-              <div className='result-row result-main pit-start'>
-                <div>Recommended Starting Fuel</div>
-                <div>{pit.fuel}</div>
-              </div>
-            :
-              <div className='pit-row'>
-                <div className='index'>
-                  #{index + 1}
-                </div>
-                <div className='pit-data'>
-                  <div>Lap</div>
-                  <div>{pit.lap}</div>
-                </div>
-                <div className='separator'></div>
-                <div className='pit-data'>
-                  <div>Fuel</div>
-                  <div>+{pit.fuel}</div>
-                </div>
-              </div>
-          )}
-        </>}
-      </div>
+      <Results
+        raceType={raceType}
+        raceLaps={raceLaps}
+        raceTime={raceTime}
+        recFuel={recFuel}
+        minFuel={minFuel}
+        pits={pits}
+      />
     </div>
   )
 }
