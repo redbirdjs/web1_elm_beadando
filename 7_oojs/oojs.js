5131619454c030ba92f71a4fbe84ec98c3868f15
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2026-04-03 14:15:02 +0200

    feladat7: gameobject class létrehozva, gameobject style hozzáadva későbbi mozgatáshoz a játéktéren

--- 7_oojs/oojs.js
+++ 7_oojs/oojs.js
@@ -1,1 +1,43 @@
-console.log("Hello világ!");
+class GameObject {
+    constructor(x, y, width, height, gameArea, name = '') {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.gameArea = gameArea;
+
+        this.element = this.create(name);
+        this.gameArea.appendChild(this.element);
+        this.render();
+    }
+
+    create(name) {
+        const div = document.createElement('div');
+        div.classList.add('game-object');
+        if (name) div.classList.add(name);
+
+        div.style.width = `${this.width}px`;
+        div.style.height = `${this.height}px`;
+        return div;
+    }
+
+    update() {}
+
+    render() {
+        this.element.style.left = `${this.x}px`;
+        this.element.style.top = `${this.y}px`;
+    }
+
+    getBounds() {
+        return {
+            x: this.x,
+            y: this.y,
+            width: this.width,
+            height: this.height
+        }
+    }
+
+    delete() {
+        if (this.element.parentNode) this.element.parentNode.removeChild(this.element);
+    }
+}
