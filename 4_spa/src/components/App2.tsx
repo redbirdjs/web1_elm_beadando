49f801a77dd61dd6b59c872eb5633ae7e07fd6ad
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2026-04-07 15:18:31 +0200

    feladat4: második app alap és css létrehozva

--- 4_spa/src/components/App2.tsx
+++ 4_spa/src/components/App2.tsx
@@ -1,8 +1,26 @@
+import { useState } from 'react'
+
+import './App2.css'
+import DisplayComponent from './App2/Display'
+import AnswerButton from './App2/AnswerButton'
+
 export default function App2() {
-  return (
-    <div>
-      <h1 style={{ color: "red" }}>app2 teszt</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis sapiente dicta iusto sunt! Dolor doloremque rem aspernatur temporibus exercitationem laboriosam non nemo perferendis nostrum, amet, reiciendis qui magni vero? Eveniet.</p>
+    const [points, setPoints] = useState(0);
+    const [timer, setTimer] = useState(60);
+
+    return (
+    <div className={'trackguessr'}>
+        <div className={'header'}>
+            <h2>Points: {points}</h2>
+            <h2>Timer: {timer}</h2>
+        </div>
+        <DisplayComponent src={'asd.png'} />
+        <div className={'answers'}>
+            <AnswerButton value={'Answer 1'} />
+            <AnswerButton value={'Answer 1'} />
+            <AnswerButton value={'Answer 1'} />
+            <AnswerButton value={'Answer 1'} />
+        </div>
     </div>
   )
 }
