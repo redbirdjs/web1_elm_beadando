66de2d643ef0110df991a85495d994a417c4f5b9
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2026-04-07 16:37:24 +0200

    feladat4: answer gomb click handler hozzáadva

--- 4_spa/src/components/App2/AnswerButton.tsx
+++ 4_spa/src/components/App2/AnswerButton.tsx
@@ -1,5 +1,5 @@
-export default function AnswerButton({ value }: { value: string }) {
+export default function AnswerButton({ value, clickHandler }: { value: string, clickHandler: (answer: string) => void }) {
     return (
-        <button className={'answer'}>{value}</button>
+        <button className={'answer'} onClick={() => clickHandler(value)}>{value}</button>
     )
 }
